45f8e5a98c1d1232246d3894bb7fbd73eb9ed864
Bleu Rubin <bleurubin@protonmail.com>
2025-10-29 01:55:08 -0400

    better message

--- src/main/java/com/bleurubin/core/domain/SoftDeleteListener.java
+++ src/main/java/com/bleurubin/core/domain/SoftDeleteListener.java
@@ -2,10 +2,15 @@ package com.bleurubin.core.domain;
 
 import jakarta.persistence.PreRemove;
 
+/**
+ * Goal is to prevent application code from accidentally deleting entities that are supposed to use
+ * soft delete
+ */
 public class SoftDeleteListener {
 
   @PreRemove
   public void preRemove(SoftDeletableEntity entity) {
-    throw new UnsupportedOperationException("Hard delete not allowed. Use markDeleted() instead.");
+    throw new UnsupportedOperationException(
+        "Hard delete not allowed for " + entity.getClass() + ". Use markDeleted() instead.");
   }
 }
