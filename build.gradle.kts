3d39f95c36b8fcd4e75c47a929ad17174a09c7ff
Bleu Rubin <bleu@budgetanalyzer.org>
2026-04-15 12:13:17 +0000

    release: bump service-common to 0.0.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val githubPackagesToken = providers.environmentVariable("GITHUB_TOKEN")
 
 allprojects {
     group = "org.budgetanalyzer"
-    version = "0.0.11"
+    version = "0.0.12"
 
     repositories {
         mavenCentral()
